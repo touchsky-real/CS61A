fcdd212608979b184e57217ae1ea69b0c7d48b3d
touchsky <syswbdyx@gmail.com>
2025-02-09 14:12:51 +0800

    finish lab09

--- Lab/lab09/lab09.scm
+++ Lab/lab09/lab09.scm
@@ -1,10 +1,29 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  ; (cond
+  ;     ((< num1 num2) -1)
+  ;     ((= num1 num2) 0)
+  ;     (else 1)
+  ; )
+  (if (< num1 num2) -1 (if (= num1 num2) 0 1))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  ; (lambda (inc) (+ num inc))
+  (define (adder inc) (+ num inc))
+  adder
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (define (f_of_g_of x) (f (g x)))
+  f_of_g_of
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  (if(= n 0)
+    (lambda (x) x)
+    (composed f (repeat f (- n 1)))
+  )
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +35,12 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b)
+  (define big (max a b))
+  (define small (min a b))
+  (define remainder (modulo big small))
+  (if (zero? remainder)
+    small
+    (gcd small remainder)
+  )
+)
